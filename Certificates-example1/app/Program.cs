62ee2f8a6f52f99afb6f303210cc5fc702d470f3
Paweł Worwąg <pawel.worwag@gmail.com>
2023-09-27 18:05:28 +0200

    First example

--- Certificates-example1/app/Program.cs
+++ Certificates-example1/app/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Security.Cryptography.X509Certificates;
 
-Console.WriteLine("Hello, World!");
+var store = new X509Store(StoreLocation.CurrentUser);
+store.Open(OpenFlags.ReadOnly);
+foreach (var cert in store.Certificates)
+{
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine($"        Subject: {cert.Subject}");
+    Console.WriteLine($"         Issuer: {cert.Issuer}");
+    Console.WriteLine($"          Valid: {cert.NotBefore} - {cert.NotAfter}");
+    Console.WriteLine($"     Thumbprint: {cert.Thumbprint}");
+    Console.WriteLine($"  Serial number: {cert.SerialNumber}");
+}
